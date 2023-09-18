7a83e782b46972df96e31d395beb4df25914539d
Qde <nzemka0@gmail.com>
2023-09-18 20:20:55 +0300

    Send emails and contact us page edit

--- send_emal.py
+++ send_emal.py
@@ -5,10 +5,10 @@ def send_email(message):
     host = "smtp.gmail.com"
     port = 465
 
-    username = "zemka.kit@gmail.com"
+    username = "#.com"
     password = "cdvgywstqvyzydjz"
 
-    receiver = "zemka.kit@gmail.com"
+    receiver = "#.com"
     context = ssl.create_default_context()
 
     with smtplib.SMTP_SSL(host, port, context=context) as server:
