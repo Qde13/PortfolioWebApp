9119a48c9cfc6a5e18ddcbece0fa66199f50687c
Qde <nzemka0@gmail.com>
2023-09-18 21:04:27 +0300

    Secured credentials withs os.getenv

--- send_emal.py
+++ send_emal.py
@@ -1,4 +1,5 @@
 import smtplib, ssl
+import os
 
 
 def send_email(message):
@@ -6,7 +7,7 @@ def send_email(message):
     port = 465
 
     username = "zemka.kit@gmail.com"
-    password = "cdvgywstqvyzydjz"
+    password = os.getenv("PASSWORD")
 
     receiver = "zemka.kit@gmail.com"
     context = ssl.create_default_context()
